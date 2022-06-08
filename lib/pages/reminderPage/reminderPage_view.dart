cf80a9295c2d4c7450e832952e403eb7dd42e97c
rebecca <rebecca.schmidt@htwg-konstanz.de>
2022-06-08 10:41:33 +0200

    Simple build of ReminderPage, how to build the page with these Widgets

--- lib/pages/reminderPage/reminderPage_view.dart
+++ lib/pages/reminderPage/reminderPage_view.dart
@@ -4,14 +4,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../util/AppColors.dart';
 import 'reminderPage_model.dart';
 
-
-class ReminderView extends ConsumerWidget {
-  const ReminderView({Key? key}) : super(key: key);
-
-
-
+class ReminderView extends StatefulWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _ReminderViewState createState() => _ReminderViewState();
+}
+
+class _ReminderViewState extends State<ReminderView> {
+  List<bool> _values = [true, false, true, false, false];
+
+  Widget build(BuildContext context) {
+    final int count = 4; //Anzahl an Switches
 
     return Scaffold(
       appBar: AppBar(
@@ -23,15 +25,36 @@ class ReminderView extends ConsumerWidget {
         ),
         centerTitle: true,
       ),
-
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'tooltip',
         backgroundColor: accentColor,
         child: Icon(Icons.add),
       ),
+      body: Column(
+        children: <Widget>[
+          for (int i = 0; i <= count; i++)
+            ListTile(
+              title: Text(
+                'Reminder ${i + 1}',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1!
+                    .copyWith(color: Colors.black),
+              ),
+              leading: Switch(
+                value: _values[i],
+                activeColor: accentColor,
+                onChanged: (bool value) {
+                  setState(() {
+                    _values[i] = value;
+                  });
+                },
+              ),
+            ),
+        ],
+      ),
     );
-
   }
 }
 
