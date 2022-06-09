27d43b97bb56b11e42bcad92bef0736b8d7e2f23
tobias-mack <tobias.mack_gg@yahoo.de>
2022-06-09 19:36:52 +0200

    HomePage: scrollable img

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -25,105 +25,129 @@ class HomeView extends ConsumerWidget {
         ),
         centerTitle: true,
       ),
-      body: Stack(
-        alignment: Alignment.topLeft,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * .6,
-            //decoration: const BoxDecoration(
-            // color: accentColor,
-            //),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: false,
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                  "assets/images/background.jpg",
+                width: double.maxFinite,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Container(
-              //decoration: BoxDecoration(
-              //  gradient: LinearGradient(
-              //    begin: Alignment.topCenter,
-              //    end: Alignment.bottomCenter,
-              //    colors: [
-              //      Colors.black.withOpacity(.1),
-              //      Colors.black.withOpacity(.9),
-              //      Colors.black.withOpacity(1),
-              //      Colors.black.withOpacity(1),
-              //      Colors.black.withOpacity(1),
-              //    ],
-              //  ),
-              //),
-              child: SafeArea(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 40),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            "All Reminders",
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                        ),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          physics: BouncingScrollPhysics(),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                          ),
-                          child: Row(
+          SliverToBoxAdapter(
+            child: Stack(
+              alignment: Alignment.topLeft,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * .6,
+                  //decoration: const BoxDecoration(
+                  // color: accentColor,
+                  //),
+                ),
+                SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Container(
+                    //decoration: BoxDecoration(
+                    //  gradient: LinearGradient(
+                    //    begin: Alignment.topCenter,
+                    //    end: Alignment.bottomCenter,
+                    //    colors: [
+                    //      Colors.black.withOpacity(.1),
+                    //      Colors.black.withOpacity(.9),
+                    //      Colors.black.withOpacity(1),
+                    //      Colors.black.withOpacity(1),
+                    //      Colors.black.withOpacity(1),
+                    //    ],
+                    //  ),
+                    //),
+                    child: SafeArea(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 40),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              ReminderTile(
-                                  image: AssetImage("assets/images/bell.PNG"),
-                                  profileName: "Profile 1"),
-                              SizedBox(width: 16),
-                              ReminderTile(
-                                  image: AssetImage("assets/images/bell.PNG"),
-                                  profileName: "Profile 2"),
-                              SizedBox(width: 16),
-                              ReminderTile(
-                                  image: AssetImage("assets/images/bell.PNG"),
-                                  profileName: "Profile 3"),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "All Reminders",
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                              ),
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                physics: BouncingScrollPhysics(),
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                ),
+                                child: Row(
+                                  children: [
+                                    ReminderTile(
+                                        image: AssetImage("assets/images/bell.PNG"),
+                                        profileName: "Profile 1"),
+                                    SizedBox(width: 16),
+                                    ReminderTile(
+                                        image: AssetImage("assets/images/bell.PNG"),
+                                        profileName: "Profile 2"),
+                                    SizedBox(width: 16),
+                                    ReminderTile(
+                                        image: AssetImage("assets/images/bell.PNG"),
+                                        profileName: "Profile 3"),
+                                  ],
+                                ),
+                              )
                             ],
                           ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            "Upcoming Reminders",
-                            style: Theme.of(context).textTheme.headline6,
+                          SizedBox(height: 16),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "Upcoming Reminders",
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            "Reminders Today",
-                            style: Theme.of(context).textTheme.headline6,
+                          SizedBox(height: 16),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "Reminders Today",
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                )
+              ],
             ),
           )
         ],
       ),
+
+
+
+
+
+
+
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'tooltip',
