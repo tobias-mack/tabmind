3ad5a3dffc54b0a6acc4ac45ad1651b5d85bf76d
tobias-mack <tobias.mack_gg@yahoo.de>
2022-07-10 14:23:04 +0200

    Local_Persistence: setup Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tabmind/pages/calendar/calendar_view.dart';
-import 'package:tabmind/pages/creationPage/creationPage_view.dart';
 import 'package:tabmind/pages/home/home_view.dart';
 import 'package:tabmind/pages/profiles/profiles_view.dart';
 import 'package:tabmind/pages/reminderPage/reminderPage_view.dart';
 import 'package:tabmind/util/AppColors.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(await buildApp());
 }
 
